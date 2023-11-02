8f4160d5c625b48b3edbcad0befe14ba77651bcf
Zandzakje <DamianTomasila@hotmail.com>
2023-11-02 14:53:28 +0100

    RabbitMQ
    messaging setup

---
+++ src/main/java/umu/kwetter/message/RabbitMQ/Config.java
@@ -0,0 +1,28 @@
+package umu.kwetter.message.RabbitMQ;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.context.annotation.Bean;
+
+public class Config {
+    public static final String topicExchangeName = "message-exchange";
+    static final String queueName = "message-queue";
+
+    @Bean
+    Queue queue() {
+        return new Queue(queueName, false);
+    }
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange(topicExchangeName);
+    }
+
+    @Bean
+    Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with("msg");
+    }
+
+}
---
+++ src/main/java/umu/kwetter/message/RabbitMQ/Runner.java
@@ -0,0 +1,4 @@
+package umu.kwetter.message.RabbitMQ;
+
+public class Runner {
+}
